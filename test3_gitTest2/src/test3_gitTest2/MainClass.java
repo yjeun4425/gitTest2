6ba0db440132ceb2b279384194c2296b8eacf133
Jieun <Jieun@DESKTOP-FMS5Q0J>
2019-02-27 16:20:02 +0900

    other repository commit test

---
+++ test3_gitTest2/src/test3_gitTest2/MainClass.java
@@ -0,0 +1,10 @@
+package test3_gitTest2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("other repository commit test");
+	}
+
+}
